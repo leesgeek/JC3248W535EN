775bf08a808d3d7f1ddc101e6c9eeab8a9c7f0e7
leesgeek <26142583+leesgeek@users.noreply.github.com>
2025-11-15 08:21:20 -0800

    Added Display Test
    Added test to evaluate display pipeline for color correctness.

---
+++ src/display_color_test.h
@@ -0,0 +1,7 @@
+/* Simple display color ordering test - three stacked rectangles */
+#ifndef DISPLAY_COLOR_TEST_H
+#define DISPLAY_COLOR_TEST_H
+
+void display_color_test_create(void);
+
+#endif // DISPLAY_COLOR_TEST_H
